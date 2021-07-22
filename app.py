3f796c258892f5bb5e1fd686ea293d5c53354955
Jems112103 <jems112103@gmail.com>
2021-07-22 19:05:05 +0000

    test

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
-
+from flask import render_template
+from flask import request
 # -- Initialization section --
 app = Flask(__name__)
-
-
+#maraidb_connection = mariadb.connect(user='chooseAUserName', password='chooseAPassword', database='Login')
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
