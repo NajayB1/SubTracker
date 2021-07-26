318cd7d37f07283a64b134b142dfa92f9edd8458
NajayB1 <najayblaze@gmail.com>
2021-07-26 18:34:47 +0000

    Update

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
 
 
 # -- Initialization section --
 app = Flask(__name__)
-
+#maraidb_connection = mariadb.connect(user='chooseAUserName', password='chooseAPassword', database='Login')
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "is this working"
+    return render_template("index.html") 
+
+@app.route('/features') 
+def features():
+    return render_template('newindex.html') 
+
+@app.route('/subscriptions') 
+def subscriptions():
+    return render_template('subscription.html')
+
+@app.route('/budgeting')  
+def budgeting():
+    return render_template('budgeting.html') 
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html') 
+
+@app.route('/home')
+def home():
+    return render_template('home.html') 
